740582e5c8b68d7c2c84ac3c3fdaef5b9f9f0d59
kuirons <wugyu@outlook.com>
2017-04-17 19:25:01 +0800

    程序初始化状态

---
+++ public/javascripts/jasmineTest.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Admin on 2017/4/17.
+ */
+/*
+"use strict"
+var _=require("lodash")
+var chai=require("chai")
+var sinon=require("sinon")
+var sinonChai=require("sinon-chai")
+var expect=chai.expect
+chai.use(sinonChai)
+
+var main=require("./test.js")
+*/
+describe("A suite of basic functions", function () {
+    it("test", function () {
+        expect("abcd").toEqual(test("abcd"));
+    });
+});
